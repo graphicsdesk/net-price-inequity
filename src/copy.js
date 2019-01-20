6b63c6fead9d7254a9e1afdde94ed3cfe777f578
Jason Kao <jasonkao85@gmail.com>
2019-01-20 06:27:16 -0500

    added copy preprocessing

--- src/copy.js
+++ src/copy.js
@@ -1,9 +1,16 @@
 export default `
+intro: Ten years ago, the act passed. Columbia required to report the average net price—
+total cost of attendance minus financial aid—for students from families in each income bracket.
+Yet taking a look at these last ten years has revealed a seemingly opposite pattern in what
+students pay for college.
+
+what they look like on the outside
+
 [steps]
 
-text: In 2008, the average net prices for students from families in the lowest two income brackets had only a fourteen dollar difference.
+text: In 2008, the average net prices for families in the $0 - $30,000 and $30,001 - $48,000 income brackets differed by only $14.
 
-text: But as Columbia’s tuition rose, these two prices have diverged in a rather unexpected way...
+text: But in the last decade, two prices have diverged in a rather unexpected way...
 
 text: The average net price for students in the $0 - $30,000 income bracket continued to rise, while that for students in the $30,001 - $48,000 income bracket remained about the same.
 
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,17 @@
+import theme from './theme';
+
 export const boundsAreEqual = (b1, b2) => {
   if (!b1 || !b2 || b1.length !== b2.length || b1.length !== 2) {
     throw new Error(`Invalid bounds given, ${b1}, ${b2}`);
   }
   return b1[0] === b2[0] && b1[1] === b2[1];
+};
+
+const income1Pattern = /\$0 - \$[\d,]{6}/g;
+const income2Pattern = /\$[\d,]{6} - \$[\d,]{6}/g;
+
+export const preprocess = copy => {
+  return copy
+    .replace(income1Pattern, `<span style="background:${theme.primary}" class="text-highlight">$&</span>`)
+    .replace(income2Pattern, `<span style="background:${theme.secondary}" class="text-highlight">$&</span>`);
 };
